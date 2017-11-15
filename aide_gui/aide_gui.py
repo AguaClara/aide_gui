dad5f09001bb768cd14d1a157bd3cb77ebb10d33
zhan zhao <zzhao17@syr.edu>
2017-11-15 11:05:22 -0500

    hard coded input fields

--- aide_gui/aide_gui.py
+++ aide_gui/aide_gui.py
@@ -2,14 +2,31 @@
 #Description-
 import adsk.core, adsk.fusion, adsk.cam, traceback
 import json
+import math
 
-# Command inputs
-_imgInputEnglish = adsk.core.ImageCommandInput.cast(None)
-_imgInputMetric = adsk.core.ImageCommandInput.cast(None)
+# Globals
+_app = adsk.core.Application.cast(None)
+_ui = adsk.core.UserInterface.cast(None)
+_units = ''
 
 # Global list to keep all event handlers in scope.
 # This is only needed with Python.
-handlers = []
+_imgInputEnglish = adsk.core.ImageCommandInput.cast(None)
+_imgInputMetric = adsk.core.ImageCommandInput.cast(None)
+_standard = adsk.core.DropDownCommandInput.cast(None)
+_pressureAngle = adsk.core.DropDownCommandInput.cast(None)
+_pressureAngleCustom = adsk.core.ValueCommandInput.cast(None)
+_backlash = adsk.core.ValueCommandInput.cast(None)
+_diaPitch = adsk.core.ValueCommandInput.cast(None)
+_module = adsk.core.ValueCommandInput.cast(None)
+_numTeeth = adsk.core.StringValueCommandInput.cast(None)
+_rootFilletRad = adsk.core.ValueCommandInput.cast(None)
+_thickness = adsk.core.ValueCommandInput.cast(None)
+_holeDiam = adsk.core.ValueCommandInput.cast(None)
+_pitchDiam = adsk.core.TextBoxCommandInput.cast(None)
+_errMessage = adsk.core.TextBoxCommandInput.cast(None)
+
+_handlers = []
 
 #parse JSON from aide_design
 def parseJsonObject(parameterJson):
@@ -21,54 +38,215 @@ def parseJsonObject(parameterJson):
     return list
 
 def run(context):
-    ui = None
     try:
-        app = adsk.core.Application.get()
-        ui  = app.userInterface
-        s = json.dumps(["p1", "p2"])
-        obj = parseJsonObject(s)
+        global _app, _ui
+        _app = adsk.core.Application.get()
+        _ui  = _app.userInterface
+        
         #ui.messageBox('Hello addin')
     
         # Get the CommandDefinitions collection.
-        cmdDefs = ui.commandDefinitions
+        cmdDefs = _ui.commandDefinitions
         
         # Create a command definition
         cmdDef = cmdDefs.addButtonDefinition('adskaide_guiPythonAddIn', 'aide_gui', 'Creates Water Treatment Plant', './resources/AIDE')        
         
         # Get the Create Panel in the model workspace
-        createPanel = ui.allToolbarPanels.itemById('SolidCreatePanel')
+        createPanel = _ui.allToolbarPanels.itemById('SolidCreatePanel')
         
         # Add the button to the bottom of the Create Panel
         AIDE_Button = createPanel.controls.addCommand(cmdDef)
         
         # Connect to the command created event.. functions to be written
-        #onCommandCreated = CommandCreatedEventHandler()
-        #AIDE_Button.commandCreated.add(onCommandCreated)
-        #handlers.append(CommandCreated)
+        onCommandCreated = GearCommandCreatedHandler()
+        cmdDef.commandCreated.add(onCommandCreated)
+        _handlers.append(onCommandCreated)
         
     except:
-        if ui:
-            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
+        if _ui:
+            _ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
 
 def stop(context):
-    ui = None
-    try:
-        app = adsk.core.Application.get()
-        ui  = app.userInterface
-        #ui.messageBox('Stop addin')
-        
-        # Cleans up the UI once add-in stopped        
-        cmdDef = ui.commandDefinitions.itemById('adskaide_guiPythonAddIn')
-        if cmdDef:
-            cmdDef.deleteMe()
-            
-        createPanel = ui.allToolbarPanels.itemById('SolidCreatePanel')
+    try:        
+        # Cleans up the UI once add-in stopped
+        createPanel = _ui.allToolbarPanels.itemById('SolidCreatePanel')
         AIDE_Button = createPanel.controls.itemById('adskaide_guiPythonAddIn')       
         if AIDE_Button:
             AIDE_Button.deleteMe()
-
+        
+        cmdDef = _ui.commandDefinitions.itemById('adskaide_guiPythonAddIn')
+        if cmdDef:
+            cmdDef.deleteMe()
+    
     except:
-        if ui:
-            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
+        if _ui:
+            _ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
+            
+class GearCommandCreatedHandler(adsk.core.CommandCreatedEventHandler):
+    def __init__(self):
+        super().__init__()
+    def notify(self, args):
+        try:
+            eventArgs = adsk.core.CommandCreatedEventArgs.cast(args)
+            
+            # Verify that a Fusion design is active.
+            des = adsk.fusion.Design.cast(_app.activeProduct)
+            if not des:
+                _ui.messageBox('A Fusion design must be active when invoking this command.')
+                return()
+                
+            defaultUnits = des.unitsManager.defaultLengthUnits
+                
+            # Determine whether to use inches or millimeters as the intial default.
+            global _units
+            if defaultUnits == 'in' or defaultUnits == 'ft':
+                _units = 'in'
+            else:
+                _units = 'mm'
+                        
+            # Define the default values and get the previous values from the attributes.
+            if _units == 'in':
+                standard = 'English'
+            else:
+                standard = 'Metric'
+            standardAttrib = des.attributes.itemByName('SpurGear', 'standard')
+            if standardAttrib:
+                standard = standardAttrib.value
+                
+            if standard == 'English':
+                _units = 'in'
+            else:
+                _units = 'mm'
+            
+            pressureAngle = '20 deg'
+            pressureAngleAttrib = des.attributes.itemByName('SpurGear', 'pressureAngle')
+            if pressureAngleAttrib:
+                pressureAngle = pressureAngleAttrib.value
+            
+            pressureAngleCustom = 20 * (math.pi/180.0)
+            pressureAngleCustomAttrib = des.attributes.itemByName('SpurGear', 'pressureAngleCustom')
+            if pressureAngleCustomAttrib:
+                pressureAngleCustom = float(pressureAngleCustomAttrib.value)            
+
+            diaPitch = '2'
+            diaPitchAttrib = des.attributes.itemByName('SpurGear', 'diaPitch')
+            if diaPitchAttrib:
+                diaPitch = diaPitchAttrib.value
+            metricModule = 25.4 / float(diaPitch)
+
+            backlash = '0'
+            backlashAttrib = des.attributes.itemByName('SpurGear', 'backlash')
+            if backlashAttrib:
+                backlash = backlashAttrib.value
+
+            numTeeth = '24'            
+            numTeethAttrib = des.attributes.itemByName('SpurGear', 'numTeeth')
+            if numTeethAttrib:
+                numTeeth = numTeethAttrib.value
+
+            rootFilletRad = str(.0625 * 2.54)
+            rootFilletRadAttrib = des.attributes.itemByName('SpurGear', 'rootFilletRad')
+            if rootFilletRadAttrib:
+                rootFilletRad = rootFilletRadAttrib.value
+
+            thickness = str(0.5 * 2.54)
+            thicknessAttrib = des.attributes.itemByName('SpurGear', 'thickness')
+            if thicknessAttrib:
+                thickness = thicknessAttrib.value
+            
+            holeDiam = str(0.5 * 2.54)
+            holeDiamAttrib = des.attributes.itemByName('SpurGear', 'holeDiam')
+            if holeDiamAttrib:
+                holeDiam = holeDiamAttrib.value
+
+            cmd = eventArgs.command
+            cmd.isExecutedWhenPreEmpted = False
+            inputs = cmd.commandInputs
+            
+            global _standard, _pressureAngle, _pressureAngleCustom, _diaPitch, _pitch, _module, _numTeeth, _rootFilletRad, _thickness, _holeDiam, _pitchDiam, _backlash, _imgInputEnglish, _imgInputMetric, _errMessage
+
+            # Define the command dialog.
+            #_imgInputEnglish = inputs.addImageCommandInput('gearImageEnglish', '', 'Resources/GearEnglish.png')
+            #_imgInputEnglish.isFullWidth = True
+
+            #_imgInputMetric = inputs.addImageCommandInput('gearImageMetric', '', 'Resources/GearMetric.png')
+            #_imgInputMetric.isFullWidth = True
+
+            _standard = inputs.addDropDownCommandInput('standard', 'Standard', adsk.core.DropDownStyles.TextListDropDownStyle)
+            if standard == "English":
+                _standard.listItems.add('English', True)
+                _standard.listItems.add('Metric', False)
+                #_imgInputMetric.isVisible = False
+            else:
+                _standard.listItems.add('English', False)
+                _standard.listItems.add('Metric', True)
+                #_imgInputEnglish.isVisible = False            
+            
+            _pressureAngle = inputs.addDropDownCommandInput('pressureAngle', 'Pressure Angle', adsk.core.DropDownStyles.TextListDropDownStyle)
+            if pressureAngle == '14.5 deg':
+                _pressureAngle.listItems.add('14.5 deg', True)
+            else:
+                _pressureAngle.listItems.add('14.5 deg', False)
+
+            if pressureAngle == '20 deg':
+                _pressureAngle.listItems.add('20 deg', True)
+            else:
+                _pressureAngle.listItems.add('20 deg', False)
+
+            if pressureAngle == '25 deg':
+                _pressureAngle.listItems.add('25 deg', True)
+            else:
+                _pressureAngle.listItems.add('25 deg', False)
+
+            if pressureAngle == 'Custom':
+                _pressureAngle.listItems.add('Custom', True)
+            else:
+                _pressureAngle.listItems.add('Custom', False)
+
+            _pressureAngleCustom = inputs.addValueInput('pressureAngleCustom', 'Custom Angle', 'deg', adsk.core.ValueInput.createByReal(pressureAngleCustom))
+            if pressureAngle != 'Custom':
+                _pressureAngleCustom.isVisible = False
+                        
+            _diaPitch = inputs.addValueInput('diaPitch', 'Diametral Pitch', '', adsk.core.ValueInput.createByString(diaPitch))   
+
+            _module = inputs.addValueInput('module', 'Module', '', adsk.core.ValueInput.createByReal(metricModule))   
+            
+            if standard == 'English':
+                _module.isVisible = False
+            elif standard == 'Metric':
+                _diaPitch.isVisible = False
+                
+            _numTeeth = inputs.addStringValueInput('numTeeth', 'Number of Teeth', numTeeth)        
+
+            _backlash = inputs.addValueInput('backlash', 'Backlash', _units, adsk.core.ValueInput.createByReal(float(backlash)))
+
+            _rootFilletRad = inputs.addValueInput('rootFilletRad', 'Root Fillet Radius', _units, adsk.core.ValueInput.createByReal(float(rootFilletRad)))
+
+            _thickness = inputs.addValueInput('thickness', 'Gear Thickness', _units, adsk.core.ValueInput.createByReal(float(thickness)))
+
+            _holeDiam = inputs.addValueInput('holeDiam', 'Hole Diameter', _units, adsk.core.ValueInput.createByReal(float(holeDiam)))
+
+            _pitchDiam = inputs.addTextBoxCommandInput('pitchDiam', 'Pitch Diameter', '', 1, True)
+            
+            _errMessage = inputs.addTextBoxCommandInput('errMessage', '', '', 2, True)
+            _errMessage.isFullWidth = True
+            '''
+            # Connect to the command related events.
+            onExecute = GearCommandExecuteHandler()
+            cmd.execute.add(onExecute)
+            _handlers.append(onExecute)        
+            
+            onInputChanged = GearCommandInputChangedHandler()
+            cmd.inputChanged.add(onInputChanged)
+            _handlers.append(onInputChanged)     
+            
+            onValidateInputs = GearCommandValidateInputsHandler()
+            cmd.validateInputs.add(onValidateInputs)
+            _handlers.append(onValidateInputs)  
+            '''
+        except:
+            if _ui:
+                _ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
+
             
             
