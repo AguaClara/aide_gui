f5585c9d2d5c2df17bdbb0505ede4624d0571433
Eldor Bekpulatov <eldorbekpualtov@dhcp-rhodes-2042.eduroam.cornell.edu>
2018-02-08 16:31:08 -0500

    Whatever we did in lab

--- scratch_gui/open_json.py
+++ scratch_gui/open_json.py
@@ -2,6 +2,6 @@ import json
 
 data = None
 
-with open('NEW_param.json') as json_file:
+with open('parameters.json') as json_file:
     data = json.load(json_file)
     print(data["tabs"][0]["some_design_name"])
--- scratch_gui/test_gui/test_gui.py
+++ scratch_gui/test_gui/test_gui.py
@@ -13,11 +13,30 @@ _units = ''
 
 
 def run(context):
-    ui = None
     try:
-        app = adsk.core.Application.get()
-        ui = app.userInterface
-        ui.messageBox('Hello addin')
+        global _app, _ui
+        _app = adsk.core.Application.get()
+        _ui = app.userInterface
+
+        # Get the CommandDefinitions collection.
+        cmdDefs = _ui.commandDefinitions
+
+        # Create a command definition
+        cmdDef = cmdDefs.addButtonDefinition('adskaide_guiPythonAddIn', 'aide_gui', 'Creates Water Treatment Plant', './resources/AIDE')
+
+        # Get the Create Panel in the model workspace
+        createPanel = _ui.allToolbarPanels.itemById('SolidCreatePanel')
+
+        # Add the button to the bottom of the Create Panel
+        AIDE_Button = createPanel.controls.addCommand(cmdDef)
+
+        # Connect to the command created event (functions to be written)
+        onCommandCreated = CommandCreatedHandler()
+        cmdDef.commandCreated.add(onCommandCreated)
+        _handlers.append(onCommandCreated)
+
+        if context['IsApplicationStartup'] == False:
+            _ui.messageBox('The "AIDE" has been added to the CREATE panel.')
 
     except:
         if ui:
