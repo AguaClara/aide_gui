5fba47393fd1c67cac298862492e4a45f70063b0
ach238 <ach238@cornell.edu>
2018-02-13 15:12:51 -0500

    yaml path

--- scratch_gui/test_gui/test_gui.py
+++ scratch_gui/test_gui/test_gui.py
@@ -10,11 +10,21 @@ _app = adsk.core.Application.cast(None)
 _ui = adsk.core.UserInterface.cast(None)
 _units = ''
 
-_numTeeth = adsk.core.StringValueCommandInput.cast(None)
+# _numTeeth = adsk.core.StringValueCommandInput.cast(None)
 
 # Event handlers
 _handlers = []
 
+sys.path.append("/Users/path-name/.local/lib/python3.6/site-packages")
+
+def abs_path(file_path):
+    """
+    Takes a relative file path to the calling file and returns the correct
+    absolute path.
+    Needed because the Fusion 360 environment doesn't resolve relative paths
+    well.
+    """
+    return os.path.join(os.path.dirname(inspect.getfile(sys._getframe(1))), file_path)
 
 # parses json and for each key; creates a global in format: _[pName]
 def createGLOBAL():
