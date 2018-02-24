3c9033b566f2b3501e041730f7dd9820f0d72e35
Eldor Bekpulatov <eldorbekpualtov@dhcp-rhodes-2318.eduroam.cornell.edu>
2018-02-24 15:50:33 -0500

    hey

--- aide_gui/test_gui/test_gui.py
+++ aide_gui/test_gui/test_gui.py
@@ -10,11 +10,19 @@ import os
 import sys
 import inspect
 from . import yaml
-from . import utilities
+
 
 # sys.dont_write_bytecode = True
 # os.environ['PYTHONDONTWRITEBYTECODE'] = '1'
 
+def abs_path(file_path):
+    """
+    Takes a relative file path to the calling file and returns the correct
+    absolute path.
+    Needed because the Fusion 360 environment doesn't resolve relative paths
+    well.
+    """
+    return os.path.join(os.path.dirname(inspect.getfile(sys._getframe(1))), file_path)
 
 # Globals
 _app = adsk.core.Application.cast(None)
