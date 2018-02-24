8983681197978b8ebbfd61fa955dff91575a0316
Eldor Bekpulatov <eldorbekpualtov@dhcp-rhodes-2318.eduroam.cornell.edu>
2018-02-24 16:00:03 -0500

    cleaned up

--- aide_gui/open_json.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open('/Users/eldorbekpualtov/Desktop/AguaClara/aide_gui/scratch_gui/new_form.json', 'r') as json_file:
-    data = json.load(json_file)
-    for param in data:
-        pName = list(param.keys())[0]
-        pAttr = param[pName]
-        globals()['_%s' % pName] = "something"
