69f9a60fbdfb8610a0f053380e27b16388795f6e
ashleyhe16 <ach238@cornell.edu>
2018-04-12 16:05:36 -0400

    Added docstrings

--- aide_gui/test_gui/test_gui.py
+++ aide_gui/test_gui/test_gui.py
@@ -13,10 +13,16 @@ from . import urllib3
 import datetime
 
 def abs_path(file_path):
-    # Takes a relative file path to the calling file and returns the correct
-    # absolute path.
-    # Needed because the Fusion 360 environment doesn't resolve relative paths
-    # well.
+    """
+    Takes a relative file path to the calling file and returns the correct
+    absolute path. Needed because the Fusion 360 environment doesn't resolve
+    relative paths well.
+
+    Parameters
+    ----------
+    file_path: str
+        Relative file path to the calling file
+    """
     return os.path.join(os.path.dirname(inspect.getfile(sys._getframe(1))), file_path)
 
 # Globals
@@ -27,10 +33,14 @@ _ui = adsk.core.UserInterface.cast(None)
 # Error Message
 _errMessage = adsk.core.TextBoxCommandInput.cast(None)
 
-# Event handlers
+# Global list to keep all event handlers in scope
 _handlers = []
 
 def run(context):
+    """
+    Creates the command definition, adds button to the bottom of the Create
+    Panel in Fusion, and connects to the command created event.
+    """
     try:
         global _app, _ui
 
@@ -44,13 +54,9 @@ def run(context):
         cmdDef = cmdDefs.addButtonDefinition('adskaide_guiPythonAddIn',
             'AIDE GUI', 'Creates Water Treatment Plant', './resources/AIDE')
 
-        # Get the Create Panel in the model workspace in Fusion
         createPanel = _ui.allToolbarPanels.itemById('SolidCreatePanel')
-
-        # Add the button to the bottom of the Create Panel in Fusion
         AIDE_Button = createPanel.controls.addCommand(cmdDef)
 
-        # Connect to the command created event (functions to be written)
         onCommandPath = CommandPathHandler()
         cmdDef.commandCreated.add(onCommandPath)
         _handlers.append(onCommandPath)
@@ -60,8 +66,10 @@ def run(context):
             _ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
 
 def stop(context):
+    """
+    Cleans up the UI once the add-in is stopped.
+    """
     try:
-        # Cleans up the UI once the add-in is stopped
         createPanel = _ui.allToolbarPanels.itemById('SolidCreatePanel')
         AIDE_Button = createPanel.controls.itemById('adskaide_guiPythonAddIn')
 
@@ -80,20 +88,26 @@ def stop(context):
             _ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
 
 class MyCommandDestroyHandler(adsk.core.CommandEventHandler):
+    """
+    When the command is done, terminate the add-in. This will release all
+    globals and remove all event handlers.
+    """
     def __init__(self):
         super().__init__()
 
     def notify(self, args):
         try:
-            # When the command is done, terminate the add-in
-            # This will release all globals and remove all event handlers
             adsk.terminate()
 
         except:
             _ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
 
-# Creates a window to request yaml local path or url from user
+
 class CommandPathHandler(adsk.core.CommandCreatedEventHandler):
+    """
+    Uses the CommandCreatedEventHandler to create a window to request yaml local
+    path or url from User.
+    """
     def __init__(self):
         super().__init__()
 
@@ -116,7 +130,11 @@ class CommandPathHandler(adsk.core.CommandCreatedEventHandler):
             if _ui:
                 _ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
 
+
 class PathLoadHandler(adsk.core.CommandEventHandler):
+    """
+    Handles when user inputs file path/URL and hits 'OK'
+    """
     def __init__(self):
         super().__init__()
 
@@ -147,8 +165,16 @@ class PathLoadHandler(adsk.core.CommandEventHandler):
             if _ui:
                 _ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
 
-# Creates the second window after retrieving parameters from inputted yaml
+
 class CommandCreatedHandler(adsk.core.CommandCreatedEventHandler):
+    """
+    Creates the second window after retrieving parameters from an inputted yaml.
+
+    Parameters
+    ----------
+    yaml_form: list
+        The design parameter list from the user inputted YAML
+    """
     def __init__(self, yaml_form):
         super().__init__()
         self.yaml_form = yaml_form
@@ -165,7 +191,7 @@ class CommandCreatedHandler(adsk.core.CommandCreatedEventHandler):
             cmd.destroy.add(onDestroy)
             _handlers.append(onDestroy)
 
-            # this creates the fields given that format file has succesfully loaded
+            # This creates the fields given that format file has succesfully loaded
             fields = createFields(inputs, self.yaml_form)
 
             # Connect to the command related events.
@@ -177,8 +203,17 @@ class CommandCreatedHandler(adsk.core.CommandCreatedEventHandler):
             if _ui:
                 _ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
 
-# Create a new window with parameters from loaded YAML
+
 class CommandExecuteHandler(adsk.core.CommandEventHandler):
+    """
+    Creates a new YAML based on user inputs and the list of fields to be
+    collected
+
+    Parameters
+    ----------
+    fields: dict
+        The design parameter list as a dictionary from the user inputted YAML
+    """
     def __init__(self, fields):
         super().__init__()
         self.fields = fields
@@ -193,15 +228,21 @@ class CommandExecuteHandler(adsk.core.CommandEventHandler):
             with open(abs_path("user_inputs_"+ current_time + ".yaml"), 'w') as outfile:
                 yaml.dump(param_values, outfile)
 
-            # Execute onfinish code here (call aide_design/aide_templates)
-
+            # Execute on finish code here (call aide_design/aide_templates)
         except:
             if _ui:
                 _ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
 
 
-# Create a yaml form structure in global called data
 def load_yaml(fpath):
+    """
+    Creates a yaml form structure in global called data
+
+    Parameters
+    ----------
+    fpath: str
+        The local file path/URL for the yaml to be retrieved from
+    """
     # If yaml is retrieved from user's local path
     try:
         with open(abs_path(fpath)) as fp:
@@ -226,10 +267,24 @@ def load_yaml(fpath):
             return None
 
 
-# Creates fields to be displayed in a new window on Fusion 360 based on
-# parameter list in input YAML to solicit parameter values from a user
 def createFields(inputs, yaml_form):
-    # Create a dict called fields to keep track of created fields
+    """
+    Creates fields to be displayed in a new window on Fusion 360 based on
+    parameter list in input YAML to solicit parameter values from a user
+
+    Parameters
+    ----------
+    inputs: CommandInputs
+        The collection of command inputs
+
+    yaml_form: list
+        The design parameter list from the user inputted YAML
+
+    Return
+    --------
+    fields: dict
+        A dictionary to keep track of created fields
+    """
     fields = {}
     # For each parameter {dictionary} in design param list
     for param in yaml_form:
@@ -237,7 +292,6 @@ def createFields(inputs, yaml_form):
         pName = list(param.keys())[0]
         # Get the value [attributes of field] of the key from the dictionary
         pAttr = param[pName]
-
         # For fields specified by attr type: "string"
         if pAttr["type"] == "string":
             # param_format(id, name, default)
@@ -258,12 +312,26 @@ def createFields(inputs, yaml_form):
         elif pAttr["type"] == "spinnerFloat":
             # param _format(id, Name, min, max, step, default)
             fields[pName] = inputs.addFloatSpinnerCommandInput(str(pName), pAttr["name"], '', pAttr["options"][0], pAttr["options"][2], pAttr["options"][1], pAttr["options"][0])
-    # returns the list of created field ids
+
     return fields
 
-# Collect inputted values from the user and adds information to a list of
-# dictionaries, each containing a parameter name and value
+
 def collectFields(fields):
+    """
+    Collect inputted values from the user and adds information to a list of
+    dictionaries, each containing a parameter name and value
+
+    Parameters
+    ----------
+    fields: list
+        The list of created field IDs from createFields
+
+    Return
+    ----------
+    params: dict
+        A dictionary with the design parameter name as keys and the user
+        inputted values as values
+    """
     params = {}
     for key, kval in fields.items():
         # If the parameter has a drop down type, the value is the selectedItem
