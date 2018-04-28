f069fcbc8aeb618fe4dc52ba5adac5b312802505
Eldor Bekpulatov <eldorbekpualtov@dhcp-gs-11063.eduroam.cornell.edu>
2018-04-28 15:30:34 -0400

    new js

---
+++ aide_gui/palette_gui/palette.js
@@ -0,0 +1,32 @@
+function sendInfoToFusion(action, src){
+    var command = {
+        type: action,
+        link: src
+    };
+    console.log(command)
+    adsk.fusionSendData('send', JSON.stringify(command));
+}
+
+
+
+window.fusionJavaScriptHandler = {handle: function(action, data){
+    try {
+        if (action == 'send') {
+            var list=JSON.parse(data);
+            console.log(list);
+        }else if (action == 'debugger') {
+            debugger;
+        }else {
+            return 'Unexpected command type: ' + action;
+        }
+    } catch (e) {
+        console.log(e);
+        console.log('exception caught with command: ' + action + ', data: ' + data);
+    }
+    return 'OK';
+}};
+
+// activate the datatable when ready
+$(document).ready( function () {
+    $('#myTable').DataTable();
+} );
