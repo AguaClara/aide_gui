fef53b0e665e95a0776f1dd759725787f7d7f69d
Eldor Bekpulatov <eldorbekpualtov@dhcp-hol-1926.eduroam.cornell.edu>
2018-05-01 16:24:55 -0400

    new templates folder

--- aide_gui/palette_gui/palette_gui.py
+++ aide_gui/palette_gui/palette_gui.py
@@ -1,8 +1,8 @@
 import os, sys, inspect, json
 import adsk.core, adsk.fusion, adsk.cam, traceback
 # add the path to local library
-sys.path.append("/Users/eldorbekpualtov/Desktop/AguaClara/aide_gui/aide_gui/palette_gui")
-from jinja2 import Template, Environment, FileSystemLoader
+# sys.path.append("/Users/eldorbekpualtov/Desktop/AguaClara/aide_gui/aide_gui/palette_gui")
+from .jinja2 import Template, Environment, FileSystemLoader
 from . import yaml
 from . import urllib3
 
@@ -19,11 +19,9 @@ def abs_path(file_path):
     return os.path.join(os.path.dirname(inspect.getfile(sys._getframe(1))), file_path)
 
 # jinjafy given the path to the template
-def render(template_path, context):
+def render(environment, template_path, context):
     path, filename = os.path.split(abs_path(template_path))
-    return Environment(
-        loader = FileSystemLoader(path or './')
-    ).get_template(filename).render(context)
+    return environment.get_template(filename).render(context)
 
 # load yaml from online path
 def load_yaml(fpath):
@@ -78,7 +76,7 @@ def jinjafy(command):
         htmlFile='template.html'
 
     # render the dictionary values onto the html file
-    result = render(htmlFile, context)
+    result = render(,htmlFile, context)
 
     # create a local html file, with jinjafied values
     with open(abs_path("jinjafied.html"), 'w') as jinjafied:
@@ -186,9 +184,12 @@ class SendInfoCommandExecuteHandler(adsk.core.CommandEventHandler):
 
 def run(context):
     try:
-        global _ui, _app
+        global _ui, _app, environment
         _app = adsk.core.Application.get()
         _ui  = _app.userInterface
+        environment = Environment(
+            loader = FileSystemLoader("./templates")
+        )
 
         # Add a command that displays the panel.
         showPaletteCmdDef = _ui.commandDefinitions.itemById('showPalette')
