4ad4cf25d1d8618b64526edfa85772a893aeb7ed
EN-CE-AC <EN-CE-AC@cornell.edu>
2018-06-14 11:19:42 -0400

    Add relative file locations

--- aide_gui.py
+++ aide_gui.py
@@ -10,19 +10,23 @@ sys.path.append(abs_path('.'))
 from .jinja2 import Environment, FileSystemLoader, select_autoescape
 from .helper import jinjafy, load_yaml
 
-link_cards = 'data/home/cards.yaml'
-link_dropdown ='data/home/dropdown.yaml'
+# Locks in file locations when calling aide > aide_gui
+BASEDIR = os.path.dirname(os.path.abspath(__file__))
+def make_path(filename, directory=BASEDIR):
+    return os.path.join(directory, filename)
+
+link_cards = make_path('data/home/cards.yaml')
+link_dropdown = make_path('data/home/dropdown.yaml')
 
 # Dropdown only needs to be fetched once
 dropdown=load_yaml(link_dropdown)
-
 
 # Global set of event handlers to keep them referenced for the duration of the command.
 handlers = []
 app = adsk.core.Application.cast(None)
 ui = adsk.core.UserInterface.cast(None)
 env = Environment(
-     loader=FileSystemLoader(abs_path('.')+'/data/templates'),
+     loader=FileSystemLoader(abs_path('.')+ '/data/templates'),
      autoescape=select_autoescape(['html', 'xml'])
  )
 
@@ -58,7 +62,7 @@ class ShowPaletteCommandExecuteHandler(adsk.core.CommandEventHandler):
                 jinjafy(env, dropdown, command)
 
                 # let palette open the jinjafied.html
-                palette = ui.palettes.add('myPalette', 'My Palette', 'jinjafied.html', True, True, True, 300, 200)
+                palette = ui.palettes.add('myPalette', 'My Palette', make_path('jinjafied.html'), True, True, True, 300, 200)
                 palette.setMinimumSize(300, 400)
                 # palette.setMaximumSize(300, 400)
 
@@ -89,13 +93,12 @@ class MyHTMLEventHandler(adsk.core.HTMLEventHandler):
             palette = ui.palettes.itemById('myPalette')
             if incoming['type'] == 'collect':
                 ui.messageBox('hello')
-                with open(abs_path("params.yaml"), 'w') as params_file:
+                with open(make_path('params.yaml'), 'w') as params_file:
                     yaml.dump(incoming['link'], params_file)
             else:
                 jinjafy(env, dropdown, incoming)
             # Set the html of the palette.
-            palette.htmlFileURL = 'jinjafied.html'
-
+            palette.htmlFileURL = make_path('jinjafied.html')
 
         except:
             ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
@@ -106,7 +109,7 @@ def run(context):
         app = adsk.core.Application.get()
         ui  = app.userInterface
 
-        importlib.reload(helper)
+        #importlib.reload(helper)
 
         # Add a command that displays the panel.
         showPaletteCmdDef = ui.commandDefinitions.itemById('showPalette')
