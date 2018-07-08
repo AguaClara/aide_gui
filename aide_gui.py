f284cb7f1b7ca74f337bafbc42a8fe15de8850fb
Oliver Leung <oal22@cornell.edu>
2018-07-08 17:46:28 -0400

    Clean up comments and refactor

--- aide_gui.py
+++ aide_gui.py
@@ -1,31 +1,21 @@
 import sys, adsk.core, traceback, json, datetime
 from importlib import reload
 from ast import literal_eval
-
-# NOTE: This can be changed to "from aide_gui.helper import display" once development on helper has been finalized. Also see "reload(helper)" and "helper.display" statements below.
 from aide_gui import helper
 
-# Allows Python to search for packages in the directory where this file is located.
+# Import local dependencies.
 sys.path.append(helper.abs_path('./dependencies'))
-
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-# Dropdown only needs to be fetched once.
-# TODO: Use back buttons for more intuitive navigation. Also see helper.display.
-dropdown = helper.load_yaml(helper.abs_path('data/dropdown.yaml'))
-
-# Global set of event handlers to keep them referenced while the palette is being run.
-handlers = []
-
-# Global set of variables to render the palette in Fusion 360.
-app = adsk.core.Application.cast(None)
+fusion360_app = adsk.core.Application.cast(None)
 ui = adsk.core.UserInterface.cast(None)
+event_handlers = []
 
-# Specify the HTML templates that are rendered through Jinja2.
-env = Environment(
+page_templates = Environment(
      loader = FileSystemLoader(helper.abs_path('data/templates')),
      autoescape = select_autoescape(['html', 'xml'])
 )
+dropdown_structure = helper.load_yaml(helper.abs_path('data/dropdown.yaml'))
 
 # Event handler for creating the command to show the palette.
 class ShowPaletteCommandCreatedHandler(adsk.core.CommandCreatedEventHandler):
@@ -36,7 +26,7 @@ class ShowPaletteCommandCreatedHandler(adsk.core.CommandCreatedEventHandler):
             command = args.command
             onExecute = ShowPaletteCommandExecuteHandler()
             command.execute.add(onExecute)
-            handlers.append(onExecute)
+            event_handlers.append(onExecute)
         except:
             ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
 
@@ -54,7 +44,7 @@ class ShowPaletteCommandExecuteHandler(adsk.core.CommandEventHandler):
             }
 
             # NOTE: This can be changed to "display(...)" once development on helper has been finalized.
-            helper.display(env, dropdown, command)
+            helper.display(page_templates, dropdown_structure, command)
 
             # Initialize the palette.
             palette = ui.palettes.add(
@@ -69,7 +59,7 @@ class ShowPaletteCommandExecuteHandler(adsk.core.CommandEventHandler):
             # Add handler to HTMLEvent of the palette.
             onHTMLEvent = MyHTMLEventHandler()
             palette.incomingFromHTML.add(onHTMLEvent)
-            handlers.append(onHTMLEvent)
+            event_handlers.append(onHTMLEvent)
         except:
             ui.messageBox('Failed: \{}'.format(traceback.format_exc()))
 
@@ -95,7 +85,7 @@ class MyHTMLEventHandler(adsk.core.HTMLEventHandler):
                     'action' : 'home',
                     'src' : helper.load_yaml(helper.abs_path('data/structure.yaml'))
                 }
-                helper.display(env, dropdown, command)
+                helper.display(page_templates, dropdown_structure, command)
                 palette = ui.palettes.itemById('aide_gui')
                 palette.htmlFileURL = helper.abs_path('data/display.html')
 
@@ -105,18 +95,18 @@ class MyHTMLEventHandler(adsk.core.HTMLEventHandler):
 
                 # Render a new page.
                 # NOTE: This can be changed to "display(...)" once development on helper has been finalized.
-                helper.display(env, dropdown, command)
+                helper.display(page_templates, dropdown_structure, command)
                 palette = ui.palettes.itemById('aide_gui')
                 palette.htmlFileURL = helper.abs_path('data/display.html')
         except:
             ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
 
-def main_run(context, onSuccess):
+def run(context, onSuccess):
     try:
         # Load global variables for use in events.
-        global ui, app, env, on_Success
-        app = adsk.core.Application.get()
-        ui  = app.userInterface
+        global ui, fusion360_app, env, on_Success
+        fusion360_app = adsk.core.Application.get()
+        ui  = fusion360_app.userInterface
         on_Success = onSuccess
         # Reload functions from helper if changes have been made.
         # NOTE: This can be deleted once development on helper has been finalized.
@@ -128,7 +118,7 @@ def main_run(context, onSuccess):
         # Connect to Command Created event.
         onCommandCreated = ShowPaletteCommandCreatedHandler()
         showPaletteCmdDef.commandCreated.add(onCommandCreated)
-        handlers.append(onCommandCreated)
+        event_handlers.append(onCommandCreated)
 
         # Open the palette as soon as the user runs the add-in.
         showPaletteCmdDef.execute()
@@ -137,7 +127,7 @@ def main_run(context, onSuccess):
         if ui:
             ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
 
-def main_stop(context):
+def stop(context):
     try:
         # Delete the palette created by this add-in.
         palette = ui.palettes.itemById('aide_gui')
